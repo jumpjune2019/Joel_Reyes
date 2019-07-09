e04d48625265dd46c18ca4d3c3a83c18f009a655
joel reyes <joel.texmed@gmail.com>
2019-07-09 12:02:15 -0500

    finished project

--- java_training/assignments/java_003/phone/src/phone/PhoneApp.java
+++ java_training/assignments/java_003/phone/src/phone/PhoneApp.java
@@ -7,42 +7,68 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+
+
 public class PhoneApp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
-		phoneBook();
+		String [][] onePhoneBook =phoneBook();
+		printToScreen(onePhoneBook);
+		printToFile(onePhoneBook);
+//		String [] array = new String[3];
+//		array=returnInput();
+//		System.out.println(array[0]+array[1]+array[2]);
 	}
-public static String[][] phoneBook() {
+public static String[][] phoneBook() throws IOException {
 	String [][] phoneBook= new String[6][3];
-	Scanner input = new Scanner(System.in);
-	String inputString ="";
-	
+	//Scanner input = new Scanner(System.in);
+	//String inputString ="";
+	String[] array=new String[3];
 	
 	phoneBook[0][0]="Name";
 	phoneBook[0][1]="Phone Number";
 	phoneBook[0][2]="City";
+	
+	
 	 for (int row = 1; row < phoneBook.length; row++) { 
-	        for (int col = 0; col < phoneBook[row].length; col++) {
-	            if(col==0){
-	                System.out.println("Enter name: ");
-	            	inputString = input.nextLine();
-	            	phoneBook[row][0]=inputString;
-	            }else if(col==1){
-	                System.out.println("Enter phone number: ");
-		            inputString = input.nextLine();
-		            phoneBook[row][1]=inputString;
-	            }else if(col==2){
-	               	System.out.println("Enter city: ");
-	            	inputString = input.nextLine();
-		            phoneBook[row][2]=inputString;
-	            }
-	            //board[row][col] = row * col; }
+	      //  for (int col = 0; col < phoneBook[row].length; col++) {
+	        	
+	        	array=returnInput();
+	        	
+	        	phoneBook[row][0]=array[0];
+	        	phoneBook[row][1]=array[1];
+	        	phoneBook[row][2]=array[2];
+//	        	if(col==0){
+//	                System.out.println("Enter name: ");
+//	            	phoneBook[row][0] = phoneInput(input);
+//	            }else if(col==1){
+//	                System.out.println("Enter phone number: ");
+//		            phoneBook[row][1] = phoneInput(input);
+//	            }else if(col==2){
+//	               	System.out.println("Enter city: ");
+//	               	phoneBook[row][2] = phoneInput(input);
+//	            }
+	        //    call for input
+	           // take input
+	           // check imput
+	            //if not null put it in array 
+	             //if null pront again fire exception
+	        
 
-		}
+		//}
+	     //print out namePhoneCity
+	        //Ask user if information is correct
+	        //row--
 	 }
+	
+	//************************************************************
+		
+	return phoneBook;
+	}
+public static void printToScreen(String[][]array) {
 	try (DataOutputStream dataOut = new DataOutputStream(new FileOutputStream("testdata"))) {
-		for (String[] row : phoneBook) {
+		for (String[] row : array) {
             for (String elem : row) {
                 System.out.format("%-15s ", elem );
             }
@@ -52,35 +78,79 @@ public static String[][] phoneBook() {
 		} catch (IOException exc) {
 		System.out.println("Write Error.");
 		}
-	//************************************************************
+}
+public static void printToFile(String[][]array) {
+	
 	//save to file
-	try (DataOutputStream dataOut = new DataOutputStream(new FileOutputStream("testdata"))) {
-		//create file
-				FileWriter fw = new FileWriter("phone.txt");
-				BufferedWriter out= new BufferedWriter(fw);
-				
-				
+		try (DataOutputStream dataOut = new DataOutputStream(new FileOutputStream("testdata"))) {
+			//create file
+					FileWriter fw = new FileWriter("phone.txt");
+					BufferedWriter out= new BufferedWriter(fw);
+					
+					
 
-				for (String[] row : phoneBook) {
-					for (String elem : row) {
-						out.write(elem +"\t \t");
+					for (String[] row : array) {
+						for (String elem : row) {
+							out.write(elem +"\t \t");
+						}
+						out.write("\n");
 					}
-					out.write("\n");
-				}
-				out.close();
+					out.close();
+			
+			
+		} catch (IOException exc) {
+			System.out.println("Write Error.");
+		}
+}
+public static String phoneInput(Scanner input) {
+	String inputString=null;
+	try {
+	
+		 inputString = input.nextLine();
+		 // do not use == to compare string it wont work
+		 if(inputString.equals("")) {
+				// how to call my custom handler
+					throw new nullContentException();
+			}
+	}catch(nullContentException exc){
+		System.out.println("input string was null"+"\n"+"Please enter right if this time: ");
+		phoneInput(input);
 		
-		
-	} catch (IOException exc) {
-		System.out.println("Write Error.");
 	}
-	return phoneBook;
+	return inputString;
+}
+public static String [] returnInput() throws IOException {
+	Scanner input = new Scanner(System.in);
+	String[]namePhoneCity=new String[3];
+	
+	
+			System.out.println("Enter Name: ");
+     	   namePhoneCity[0]=phoneInput(input);
+     	  System.out.println("Enter Phone: ");
+     	   namePhoneCity[1]=phoneInput(input);
+     	  System.out.println("Enter City: ");
+     	   namePhoneCity[2]=phoneInput(input);
+        
+		System.out.println("Is this right?");
+		
+	         System.out.println("Name: "+ namePhoneCity[0]
+	        			   	   +" Phone: "+ namePhoneCity[1]
+	        			   	   +" City: "+ namePhoneCity[2]);
+	         System.out.println("Press (y) for yes (n) for to re-enter information");
+	      // Read a character from the keyboard.
+	         String key;
+	 		key =  input.nextLine(); // get a char
+	 		if (key.equals("y") ) {
+	 			return namePhoneCity;
+	 		}else if(key.equals("n") ) {
+	 			returnInput();
+	 		}else {
+	 			System.out.println("that was not an options lets make sure we start again");
+	 			returnInput();
+	 		}
+   
+	return namePhoneCity;
 }
 }
-/*
- * for (String[] row : matrix) {
-            for (String elem : row) {
-                System.out.format("%-23s ", elem );
-            }
-            System.out.println();
-        }*/
+
  
