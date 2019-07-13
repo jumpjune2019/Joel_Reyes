63f08d45b46416604698a06b8009b5a5c5b9a9c4
joel reyes <joel.texmed@gmail.com>
2019-07-12 20:27:27 -0500

    goals for saturday night

---
+++ java_training/assignments/java_005/Extra/src/Money.java
@@ -0,0 +1,13 @@
+public class Money {
+    Money(){}
+
+    // execption
+    //setPenny(){}
+    // setNichel(){}
+    // setDime(){}
+    // setDollar(){}
+    //returnRaw(){}
+    // returnPretty(){}
+    //returnCurrenty(){}
+
+}
